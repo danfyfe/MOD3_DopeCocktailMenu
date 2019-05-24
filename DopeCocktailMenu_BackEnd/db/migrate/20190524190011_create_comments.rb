b27a751680f1bc70f73f1cb0148c2d3b0defeb13
axionos <axionoso@gmail.com>
2019-05-24 15:58:26 -0400

    create seed data

--- DopeCocktailMenu_BackEnd/db/migrate/20190524190011_create_comments.rb
+++ DopeCocktailMenu_BackEnd/db/migrate/20190524190011_create_comments.rb
@@ -3,6 +3,7 @@ class CreateComments < ActiveRecord::Migration[5.2]
     create_table :comments do |t|
       t.integer :cocktail_id
       t.string :content
+      t.string :username
       t.belongs_to :cocktail, foreign_key: true
       t.timestamps
     end
