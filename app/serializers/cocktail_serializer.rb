c807bf73eba8c1e548d1e81d99ef7ac38bb112ae
danfyfe <fyfedaniel@gmail.com>
2019-05-28 13:50:00 -0400

    add description

--- app/serializers/cocktail_serializer.rb
+++ app/serializers/cocktail_serializer.rb
@@ -1,5 +1,5 @@
 class CocktailSerializer < ActiveModel::Serializer
-  attributes :id, :mood_id, :name, :recipe, :img_url
+  attributes :id, :mood_id, :name, :recipe, :img_url, :description
   has_many :likes
   has_many :comments
   belongs_to :mood
