cf3f09bb17f9218c66740abaa421527c9ad627f4
axionos <axionoso@gmail.com>
2019-05-28 17:06:03 -0400

    add likes and comments routes

--- app/controllers/api/v1/likes_controller.rb
+++ app/controllers/api/v1/likes_controller.rb
@@ -1,7 +1,20 @@
 class Api::V1::LikesController < ApplicationController
-  # def create
-  #   @like = Like.create()
-  #
-  # end
+
+  def index
+    @likes = Like.all
+    render json: @likes
+  end
+
+  def create
+    @like = Like.create(like_params)
+    @cocktail = Cocktail.find(params[:cocktail_id])
+    # render json: @cocktail
+  end
+
+  private
+
+  def like_params
+    params.permit(:like).require(:cocktail_id)
+  end
 
 end
--- config/routes.rb
+++ config/routes.rb
@@ -5,6 +5,8 @@ Rails.application.routes.draw do
     namespace :v1 do
       resources :moods, only:[:index,:show]
       resources :cocktails, only:[:index,:show]
+      resources :likes, only:[:index, :create]
+      resources :comments, only:[:index, :create]
     end
   end
 end
